94ad7a3a8ff50097e1f0751fad7e47e3e8122bd9
Alok Mishra7275 <mishraalok7275@gmail.com>
2026-03-21 11:58:21 +0530

    fix my environment viriabless

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,14 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// In Prisma 7, we don't pass ANY arguments to the constructor.
-// It automatically looks for 'prisma.config.ts' in your root.
 export const prisma =
   globalForPrisma.prisma ||
-  new PrismaClient()
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
