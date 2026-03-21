ab17f5d268094655f5b157ff57bdcde282518c63
Alok Mishra7275 <mishraalok7275@gmail.com>
2026-03-21 11:32:21 +0530

    fix: use named export for prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-})
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export default prisma
+// We export it as a NAMED constant 'prisma'
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
